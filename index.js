d0fce29723fb85f35b0c0f355cac3710e35412e6
Seth Malaki <seth.malaki@gmail.com>
2015-04-15 13:31:50 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// https://www.npmjs.com/package/mysql
+var mysql = require('mysql');
+// https://www.npmjs.com/package/csv
+var csv = require('csv');
+// https://www.npmjs.com/package/q
+var q = require('q');
+// http://csv.adaltas.com/parse/examples/
+var parse = require('csv-parse');
+
+var fs = require('fs');
+
+
+var parser = parse({delimiter: ','}, function(err, data){
+  if(err) {
+  	console.log("CSV error.")
+  	process.exit(1);
+  } else {
+  	data.forEach(function(row, index)  {
+  		console.log(row);
+  	});
+  }
+});
+
+fs.createReadStream(__dirname+'/test.csv').pipe(parser);
